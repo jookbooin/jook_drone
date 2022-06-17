2acf42a6ad555f6426816da09fd1549c6305f7d4
jookbooin <3670lsh@naver.com>
2022-06-17 17:28:16 +0900

    0617

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jookbooin/jook_drone/src
+# Build directory: /home/jookbooin/jook_drone/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("yoonsewon_description")
---
+++ build/yoonsewon_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jookbooin/jook_drone/src/yoonsewon_description
+# Build directory: /home/jookbooin/jook_drone/build/yoonsewon_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
